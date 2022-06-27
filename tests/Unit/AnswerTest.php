d7c3c2633d9e83c9c0e5c6c2b18f31f1c0ade8c1
jiuxiaoer <yinshen@79xj.cn>
2022-06-27 17:40:59 +0800

    cancel vote up

--- tests/Unit/AnswerTest.php
+++ tests/Unit/AnswerTest.php
@@ -62,4 +62,21 @@ class AnswerTest extends TestCase
         ]);
     }
 
+    /** @test */
+    public function can_cancel_vote_up_an_answer()
+    {
+        $this->signIn();
+
+        $answer = create(Answer::class);
+
+        $answer->voteUp(auth()->user());
+
+        $answer->cancelVoteUp(auth()->user());
+
+        $this->assertDatabaseMissing('votes', [
+            'user_id' => auth()->id(),
+            'voted_id' => $answer->id,
+            'voted_type' => get_class($answer)
+        ]);
+    }
 }
